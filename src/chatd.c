b94f597cfa4279a2b87432d1daca1a099279115b
Dadi <dadi15@ru.is>
2016-11-02 01:31:22 +0000

    Added certificates and certificate setup methods

--- src/chatd.c
+++ src/chatd.c
@@ -6,19 +6,46 @@
  * select on how to do this (Hint: Iterate with FD_ISSET()).
  */
 
-#include <sys/select.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <sys/types.h> 
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+ #include <assert.h>
+ #include <sys/select.h>
+ #include <sys/socket.h>
+ #include <sys/time.h>
+ #include <sys/types.h>
+ #include <netinet/in.h>
+ #include <string.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <arpa/inet.h>
+ #include <openssl/crypto.h>
+ #include <openssl/ssl.h>
+ #include <openssl/err.h>
+ #include <openssl/engine.h>
+ #include <openssl/conf.h>
+ #include <glib.h>
+ #include <time.h>
+ #include <signal.h>
+ #include <errno.h>
+ #include <sys/types.h>
 
-#include <glib.h>
 
+ #define CONNECTED "connected"
+ #define DISCONNECTED "disconnected"
+ #define AUTHENTICATED "authenticated"
+ #define AUTHENTICATION_ERROR "authentication error"
+ #define TIMED_OUT "timed out"
+ #define WELCOME "Welcome"
+
+ #define TIMEOUT_SEC 120
+
+ #define PASSWORDS_FILE "passwords.ini"
+ #define CERTIFICATE_FILE "fd.crt"
+ #define PRIVATE_KEY_FILE "fd.key"
+
+ #define UNUSED(x) (void)(x)
+ #define MAX_CLIENTS 5
+
+ SSL_CTX *ssl_ctx;
 
 /* This can be used to build instances of GTree that index on
    the address of a connection. */
@@ -50,17 +77,40 @@ gint fd_cmp(gconstpointer fd1,  gconstpointer fd2, gpointer G_GNUC_UNUSED data)
 
 int main(int argc, char **argv)
 {
-     struct sockaddr_in server, client;
+    // Install the sigint handler
+    signal(SIGINT, sigint_handler); /* ctrl-c */
+    printf("Number of arguments %d\n", argc);
+    printf("Portnumber : %s\n", argv[1]);
 
-     if (argc != 2) 
-     {
-          fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-          exit(EXIT_FAILURE);
-     }
+    SSL_library_init(); /* load encryption & hash algorithms for SSL */
+    SSL_load_error_strings(); /* load the error strings for good error reporting */
+    ssl_ctx = SSL_CTX_new(TLSv1_method()); // initilize ssl context
+    //int my_port = atoi(argv[1]);
 
-     const int server_port = strtol(argv[1], NULL, 10);
+    //struct sockaddr_in server, client;
 
-     /* Initialize OpenSSL */
+    if ( !ssl_ctx ) {
+        ERR_print_errors_fp(stderr);
+        exit(1);
+    }
+
+    // Load certificate file into the structure
+    if (SSL_CTX_use_certificate_file(ssl_ctx, CERTIFICATE_FILE, SSL_FILETYPE_PEM) <= 0) {
+        printf("Error loading certificate file");
+        ERR_print_errors_fp(stderr);
+        exit(1);
+    }
+    // Load private key file into the structure
+    if (SSL_CTX_use_PrivateKey_file(ssl_ctx, PRIVATE_KEY_FILE, SSL_FILETYPE_PEM) <= 0) {
+        printf("Error loading private key");
+        ERR_print_errors_fp(stderr);
+        exit(1);
+    }
+
+    if ( !SSL_CTX_check_private_key(ssl_ctx) ) {
+        printf("Private key does not match the certificate public key\n");
+        exit(1);
+    }
 
      /* Receive and handle messages. */
 
