1fceeb7491b88b7e9b2c3698eb7d296593454163
JonSteinn <jonsteinn@gmail.com>
2016-11-02 23:03:25 +0000

    cleanup

--- src/chatd.c
+++ src/chatd.c
@@ -30,95 +30,20 @@
 #define CERTIFICATE "encryption/fd.crt"
 #define PRIVATE_KEY "encryption/fd.key"
 
-#define UNUSED(x) (void)(x)
-
-static GTree* chat_room_tree;
-static GTree* client_tree;
-
-SSL_CTX *ssl_ctx;
-
-void sigint_handler(int sig)
-{
-	UNUSED(sig);
-	g_tree_destroy(chat_room_tree);
-	g_tree_destroy(client_tree);
-	SSL_CTX_free(ssl_ctx);
-	RAND_cleanup();
-	ENGINE_cleanup();
-	CONF_modules_unload(1);
-	CONF_modules_free();
-	EVP_cleanup();
-	ERR_free_strings();
-	ERR_remove_state(0);
-	CRYPTO_cleanup_all_ex_data();
-	exit(0);
-}
+#define MAX_QUEUED 2
 
 void exit_error(char *msg)
-{
-	perror(msg);
-	exit(EXIT_FAILURE);
-}
-
-/* This can be used to build instances of GTree that index on the address of a connection. */
-int sockaddr_in_cmp(const void *addr1, const void *addr2)
-{
-	const struct sockaddr_in *_addr1 = addr1;
-	const struct sockaddr_in *_addr2 = addr2;
-
-	/* If either of the pointers is NULL or the addresses belong to different families, we abort. */
-	g_assert((_addr1 == NULL) || (_addr2 == NULL) || (_addr1->sin_family != _addr2->sin_family));
-	if (_addr1->sin_addr.s_addr < _addr2->sin_addr.s_addr) return -1;
-	else if (_addr1->sin_addr.s_addr > _addr2->sin_addr.s_addr) return 1;
-	else if (_addr1->sin_port < _addr2->sin_port) return -1;
-	else if (_addr1->sin_port > _addr2->sin_port) return 1;
-	return 0;
-}
-
-
-/* This can be used to build instances of GTree that index on
-   the file descriptor of a connection. */
-gint fd_cmp(gconstpointer fd1,  gconstpointer fd2, gpointer G_GNUC_UNUSED data)
-{
-	return GPOINTER_TO_INT(fd1) - GPOINTER_TO_INT(fd2);
-}
-
-/* Sets up socket and error checks while doing so. We allocate the 
- * sockaddr_in struct's resources after this functions since we do 
- * not need it elsewhere.
- *
- * Paramaters: port number of server
- * Return value: file descriptor of server */
 int init_server(int port)
-{
-	int socket_fd;
-	if ((socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) exit_error("socket");
-	
-	struct sockaddr_in server;
-	memset(&server, 0, sizeof(server));
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = htonl(INADDR_ANY);
-	server.sin_port = htons(port);
-
-	if (bind(socket_fd, (struct sockaddr *)&server, (socklen_t)sizeof(server)) < 0) exit_error("bind");
-	if (listen(socket_fd, 1) < 0) exit_error("listen");
-
-	return socket_fd;
-}
-
 
 int main(int argc, char **argv)
 {
 	if (argc < 2) exit_error("argument");
 	const int server_port = strtol(argv[1], NULL, 0);
 
-	// Install the sigint handler for key interupt
-	signal(SIGINT, sigint_handler);
 	SSL_library_init();
 	SSL_load_error_strings();
-	ssl_ctx = SSL_CTX_new(TLSv1_method());
-	
-	if (ssl_ctx == NULL) exit_error("ssl_ctx");
+	SSL_CTX *ssl;
+	if ((ssl = SSL_CTX_new(TLSv1_method())) == NULL) exit_error("SSL CTX");
 	if (SSL_CTX_use_certificate_file(ssl_ctx, CERTIFICATE, SSL_FILETYPE_PEM) <= 0) exit_error("certificate");
 	if (SSL_CTX_use_PrivateKey_file(ssl_ctx, PRIVATE_KEY, SSL_FILETYPE_PEM) <= 0) exit_error("privatekey");
 	if (SSL_CTX_check_private_key(ssl_ctx) != 1) exit_error("match");
@@ -143,4 +68,34 @@ int main(int argc, char **argv)
 	}
 	
 	exit(EXIT_SUCCESS);
+}
+
+/* Prints msg to standard error and exits with error code EXIT_FAILURE */
+void exit_error(char *msg)
+{
+	perror(msg);
+	exit(EXIT_FAILURE);
+}
+
+/* Sets up socket and error checks while doing so. We allocate the 
+ * sockaddr_in struct's resources after this functions since we do 
+ * not need it elsewhere.
+ *
+ * Paramaters: port number of server
+ * Return value: file descriptor of server */
+int init_server(int port)
+{
+	int socket_fd;
+	if ((socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) exit_error("socket");
+	
+	struct sockaddr_in server;
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_port = htons(port);
+
+	if (bind(socket_fd, (struct sockaddr *)&server, (socklen_t)sizeof(server)) < 0) exit_error("bind");
+	if (listen(socket_fd, MAX_QUEUED) < 0) exit_error("listen");
+
+	return socket_fd;
 }
